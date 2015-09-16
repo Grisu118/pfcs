f47f6efa15ae9ae76511d223041f984f0125e7e3
Benjamin Leber <grisu118@grisu118.ch>
2015-09-16 13:08:50 +0200

    Init

---
+++ src/MyFirst2D.java
@@ -0,0 +1,68 @@
+//  -------------   JOGL 2D-Programm  -------------------
+import javax.media.opengl.*;
+
+public class MyFirst2D extends GLBase1
+{
+
+    //  ---------  globale Daten  ---------------------------
+
+    float left=-1, right=1;             // ViewingVolume
+    float bottom=-1, top=1;
+    float near=-10, far=1000;
+
+   //  ---------  Methoden  ----------------------------------
+
+    public void zeichneDreieck(GL4 gl,
+                        float x1, float y1, float z1,
+                        float x2, float y2, float z2,
+                        float x3, float y3, float z3)
+    {  rewindBuffer(gl);
+       putVertex(x1,y1,z1);           // Eckpunkte in VertexArray speichern
+       putVertex(x2,y2,z2);
+       putVertex(x3,y3,z3);
+       int nVertices = 3;
+       copyBuffer(gl, nVertices);
+       gl.glDrawArrays(GL4.GL_TRIANGLES, 0, nVertices);
+    }
+
+    //  ----------  OpenGL-Events   ---------------------------
+
+    @Override
+    public void init(GLAutoDrawable drawable)
+    {  super.init(drawable);
+       GL4 gl = drawable.getGL().getGL4();
+       gl.glClearColor(0,0,1,1);                         // Hintergrundfarbe (RGBA)
+       gl.glDisable(GL4.GL_DEPTH_TEST);                  // ohne Sichtbarkeitstest
+    }
+
+
+    @Override
+    public void display(GLAutoDrawable drawable)
+    { GL4 gl = drawable.getGL().getGL4();
+      gl.glClear(GL4.GL_COLOR_BUFFER_BIT);
+      setColor(1,1,1);
+      drawAxis(gl, 8,8,8);             //  Koordinatenachsen
+      setColor(1,0,0);
+      zeichneDreieck(gl,-0.5f,-0.5f,0, 0.5f,-0.5f,0, 0,0.5f,0);
+    }
+
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y,
+                        int width, int height)
+    {  GL4 gl = drawable.getGL().getGL4();
+       // Set the viewport to be the entire window
+       gl.glViewport(0, 0, width, height);
+
+       // -----  Projektionsmatrix festlegen  -----
+       setOrthogonalProjection(gl,left,right,bottom,top,near,far);
+    }
+
+
+    //  -----------  main-Methode  ---------------------------
+
+    public static void main(String[] args)
+    { new MyFirst2D();
+    }
+
+}
---
+++ src/MyRenderer1.java
@@ -0,0 +1,71 @@
+//  --------  Interface to OpenGL 3.0  --------------
+//                                        E. Gutknecht Juli 2015
+import javax.media.opengl.*;
+import javax.media.opengl.awt.*;
+import ch.fhnw.util.math.*;
+
+
+public interface MyRenderer1
+{
+
+    public GLCanvas getGLCanvas();                         // OpenGL-Window
+
+    //  --------  Vertex-Methoden  -----------
+
+    public void setColor(float r, float g, float b);       // aktuelle Vertexfarbe setzen
+
+    void putVertex(float x, float y, float z);             // Vertex-Daten in Buffer speichern
+
+    public void copyBuffer(GL gl,int nVertices);           // Vertex-Array in OpenGL-Buffer kopieren
+
+    public void rewindBuffer(GL gl);                       // Bufferposition zuruecksetzen
+
+
+    //  --------  ModelView-Transformation  -----------
+
+    //  ------------  Kamera-System  --------------------
+
+    public void setCameraSystem(GL4 gl, float r,                                     // Abstand der Kamera von O
+                                float elevation,                                     // Elevationswinkel in Grad
+                                float azimut);                                       // Azimutwinkel in Grad
+
+
+    public void setCameraSystem(GL4 gl, Vec3 A, Vec3 B, Vec3 up);                    // LookAt-Positionierung
+
+
+    //  ---------  Operationen fuer ModelView-Matrix  --------------------
+
+    public void rotate(GL4 gl, float phi, float x, float y, float z);                 // Objekt-System drehen, phi in Grad
+
+    public void translate(GL4 gl, float x, float y, float z);                         // Objekt-System verschieben
+
+    public void scale(GL4 gl, float scale);                                           // Skalierung Objekt-System
+
+    public void rotateCam(GL4 gl, float phi, float x, float y, float z);              // Kamera-System drehen, phi in Grad
+
+    public void translateCam(GL4 gl, float x, float y, float z);                      // Kamera-System verschieben
+
+    public void pushMatrix(GL4 gl);                                                   // ModelView-Matrix speichern
+
+    public void popMatrix(GL4 gl);                                                    // ModelView-Matrix vom Stack holen
+
+    public Mat4 getModelViewMatrix(GL4 gl);
+
+    public void setModelViewMatrix(GL4 gl, Mat4 M);
+
+    public void loadIdentity(GL4 gl);                                                 // Rueckstellung auf Einheitsmatrix
+
+
+    //  ---------  Projektion auf Bildebene -------------------
+
+    public void setOrthogonalProjection(GL4 gl, float left, float right,           // Grenzen des ViewingVolumes
+                                      float bottom, float top,
+                                      float near, float far);
+
+
+    public void setPerspectiveProjection(GL4 gl, float left, float right,          // Grenzen des ViewingVolumes
+                                      float bottom, float top,
+                                      float near, float far);
+
+
+}
