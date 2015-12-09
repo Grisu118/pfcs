061b58a117297febb85f62de1f730ef264087b05
Benjamin Leber <grisu118@grisu118.ch>
2015-12-09 13:15:38 +0100

    update dynamics

--- src/ch/fhnw/util/math/Dynamics.java
+++ src/ch/fhnw/util/math/Dynamics.java
@@ -5,9 +5,45 @@ package ch.fhnw.util.math;
  */
 public abstract class Dynamics {
 
+    /** Vector field */
+    abstract double[] f(double[] x);
+
     public double[] euler(double[] x, double dt) {
-        return f(x);
+        double[] y = f(x);
+        double[] xx = new double[x.length];
+        for (int i = 0; i < x.length; i++) {
+            xx[i] = x[i] + y[i]*dt;
+        }
+        return xx;
     }
 
-    abstract double[] f(double[] x);
+    /**
+     * Runge-Kutta
+     * @param x
+     * @param dt
+     * @return
+     */
+    public double[] runge(double[] x, double dt) {
+        double dt2 = (dt/2);
+        double[] y1 = f(x);
+        double[] xx = new double[x.length];
+        for (int i = 0; i < x.length; i++) {
+            xx[i] = x[i] + y1[i]*dt2;
+        }
+        double[] y2 = f(xx);
+        for (int i = 0; i < x.length; i++) {
+            xx[i] = x[i] + y2[i]*dt2;
+        }
+        double[] y3 = f(xx);
+        for (int i = 0; i < x.length; i++) {
+            xx[i] = x[i] + y3[i]*dt;
+        }
+        double[] y4 = f(xx);
+        double y;
+        for (int i = 0; i < x.length; i++) {
+            y = (y1[i]+2*y2[i]+2*y3[i]+y4[i])/6;
+            xx[i] = x[i] + y*dt;
+        }
+        return xx;
+    }
 }
