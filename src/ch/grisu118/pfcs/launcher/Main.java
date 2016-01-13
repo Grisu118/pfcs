7b545187fdde086704c23aa8c66ab4563269a1e3
Benjamin Leber <grisu118@grisu118.ch>
2016-01-13 14:45:42 +0100

    fix launcher close

--- src/ch/grisu118/pfcs/launcher/Main.java
+++ src/ch/grisu118/pfcs/launcher/Main.java
@@ -2,12 +2,14 @@ package ch.grisu118.pfcs.launcher;
 
 import ch.grisu118.pfcs.launcher.view.LauncherController;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
@@ -51,6 +53,11 @@ public class Main extends Application {
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
             primaryStage.show();
+            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                public void handle(WindowEvent we) {
+                    System.exit(0);
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
